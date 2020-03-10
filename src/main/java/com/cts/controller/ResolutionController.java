e0916ba639ac072e63ad873a91b61260f5d6b532
839835 <839835@PC182306.cts.com>
2020-03-10 17:34:43 +0530

    dgf

--- src/main/java/com/cts/controller/ResolutionController.java
+++ src/main/java/com/cts/controller/ResolutionController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -13,6 +14,7 @@ import com.cts.dao.raiseissuedao;
 import com.cts.model.CategoryBean;
 import com.cts.model.RaiseIssueBean;
 
+@Controller
 public class ResolutionController {
 
 	@Autowired
